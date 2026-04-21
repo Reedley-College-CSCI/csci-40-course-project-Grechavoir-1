3f4f0cea009f25a5545c43f25c8061bc7cd20fa1
Jaxon Ahumada <jaxahumada@gmail.com>
2026-04-21 10:13:13 -0700

    update author name and begin project description in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 // CSCI-40 Final Project
-// Author: [Your Name Here]
+// Author: Jaxon Ahumada
 
 /* 
- * Description:
- *
+ My project is a space exploration journey where the user will be navigating in a spaceship and can drop off and pick up materials on different planets and space stations.  
+The spacecraft will have a fuel that must be refilled to restrict unlimited distance.  
+The system will track all changes to the stations on the different locations.  
+The system will also keep track of how long each travel was and determine the amount of fuel used.
+The system will keep track of every visit and also save every material dropped off and picked up in each location.  
+I don't know if I want to add an a cost system or not but if I do I would keep a system for the cost of every material and any price changes on them as well as money spent of fuel.
 */
 
 #include <iostream>
