83737d45003e26be08cda285322a07c6f76ba871
Jaxon Ahumada <jaxahumada@gmail.com>
2026-04-21 10:15:27 -0700

    Added using namespace std and prompted user for their name. Also added cout statements to introduce the user to the program.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 // Author: Jaxon Ahumada
 
 /* 
- My project is a space exploration journey where the user will be navigating in a spaceship and can drop off and pick up materials on different planets and space stations.  
+My project is a space exploration journey where the user will be navigating in a spaceship and can drop off and pick up materials on different planets and space stations.  
 The spacecraft will have a fuel that must be refilled to restrict unlimited distance.  
 The system will track all changes to the stations on the different locations.  
 The system will also keep track of how long each travel was and determine the amount of fuel used.
@@ -16,6 +16,20 @@ I don't know if I want to add an a cost system or not but if I do I would keep a
 #include <iomanip>
 #include <fstream>
 
+using namespace std;
+
 int main() {
+    cout << "Hello..." << endl << "Enter your name:" << endl;
+
+    string charName;
+    getline(cin, charName);
+
+    cout << "Welcome to the Space Exploration Journey!" << endl;
+    cout << "You have been drafted to venture the cosmos and"
+         << " be a hero for your home planet!" << endl;
+    cout << "Do not fear the darkness of space, but look forward to the"
+         << " nearest star!" << endl;
+    cout << "Godspeed " << charName << "!" << endl;
+
     return 0;
 }
